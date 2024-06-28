1f34a7e6ff3f7146b468227545e7242f0d0797ca
Franco Caivano <cfranco3102@gmail.com>
2024-06-28 15:42:48 -0300

    Terminamos el index

--- js/index.js
+++ js/index.js
@@ -1,91 +1,182 @@
-
 let selector = document.querySelector(".selector");
 let pizarra = document.getElementById("pizarra");
+let horaActualizacion = document.getElementById("parrafo");
 
 
+const links = [
+  "https://dolarapi.com/v1/dolares",
+  "https://dolarapi.com/v1/dolares/oficial",
+  "https://dolarapi.com/v1/dolares/blue",
+  "https://dolarapi.com/v1/dolares/bolsa",
+  "https://dolarapi.com/v1/dolares/contadoconliqui",
+  "https://dolarapi.com/v1/dolares/tarjeta",
+  "https://dolarapi.com/v1/dolares/mayorista",
+  "https://dolarapi.com/v1/dolares/cripto",
+  "https://dolarapi.com/v1/cotizaciones/eur",
+  "https://dolarapi.com/v1/cotizaciones/brl",
+  "https://dolarapi.com/v1/cotizaciones/clp",
+  "https://dolarapi.com/v1/cotizaciones/uyu",
+];
 
-selector.addEventListener("change", function(){
+document.addEventListener("DOMContentLoaded", function () {
+  for (i = 1; i < links.length; i++){
     
-    switch(selector.value){
-        case "0":
-        pizarra.innerHTML = `
-        <article class="moneda"><p class="title-article-moneda">OFICIAL</p><img src="./img/united-states.png" class="bandera">
-            <div class="precio">
-              <div class="precio-compra">
-                <p>COMPRA</p>
-              <p>$1000</p>
-              </div>
-              <div class="precio-venta">
-                <p>VENTA</p>
-              <p>$1030</p>
-              </div>
-              </div>
-          <input type="checkbox" id="star-checkbox1" class="input-star-checkbox"><label for="star-checkbox1" class="star"><span class="material-symbols-outlined" style="font-size: 28px;">star</span></label></article>
-          <article class="moneda"><p class="title-article-moneda">BLUE</p><img src="./img/european-union.png" class="bandera">
-            <div class="precio">
-              <div class="precio-compra">
-                <p>COMPRA</p>
-              <p>$1000</p>
-              </div>
-              <div class="precio-venta">
-                <p>VENTA</p>
-              <p>$1030</p>
-              </div>
-              </div>
-          <input type="checkbox" id="star-checkbox2" class="input-star-checkbox"><label for="star-checkbox2" class="star"><span class="material-symbols-outlined" style="font-size: 28px;">star</span></label></article>
-          <article class="moneda"><p class="title-article-moneda">TARJETA</p><img src="./img/flag.png" class="bandera">
-            <div class="precio">
-              <div class="precio-compra">
-                <p>COMPRA</p>
-              <p>$1000</p>
-              </div>
-              <div class="precio-venta">
-                <p>VENTA</p>
-              <p>$1030</p>
-              </div>
-              </div>
-          <input type="checkbox" id="star-checkbox3" class="input-star-checkbox"><label for="star-checkbox3" class="star"><span class="material-symbols-outlined" style="font-size: 28px;">star</span></label></article>
-          <article class="moneda"><p class="title-article-moneda">MEP</p><img src="./img/chile.png" class="bandera">
-            <div class="precio">
-              <div class="precio-compra">
-                <p>COMPRA</p>
-              <p>$1000</p>
-              </div>
-              <div class="precio-venta">
-                <p>VENTA</p>
-              <p>$1030</p>
-              </div>
-              </div>
-          <input type="checkbox" id="star-checkbox4" class="input-star-checkbox"><label for="star-checkbox4" class="star"><span class="material-symbols-outlined" style="font-size: 28px;">star</span></label></article>
-          <article class="moneda"><p class="title-article-moneda">CCL</p><img src="./img/uruguay.png" class="bandera">
-            <div class="precio">
-              <div class="precio-compra">
-                <p>COMPRA</p>
-              <p>$1000</p>
-              </div>
-              <div class="precio-venta">
-                <p>VENTA</p>
-              <p>$1030</p>
-              </div>
-              </div>
-          <input type="checkbox" id="star-checkbox5" class="input-star-checkbox"><label for="star-checkbox5" class="star"><span class="material-symbols-outlined" style="font-size: 28px;">star</span></label></article>
-          <article class="moneda"><div class="bandera"><img src=""></div><p class="title-article-moneda">CRIPTO</p>
-            <div class="precio">
-              <div class="precio-compra">
-                <p>COMPRA</p>
-              <p>$1000</p>
-              </div>
-              <div class="precio-venta">
-                <p>VENTA</p>
-              <p>$1030</p>
-              </div>
-              </div>
-          <input type="checkbox" id="star-checkbox6" class="input-star-checkbox"><label for="star-checkbox6" class="star"><span class="material-symbols-outlined" style="font-size: 28px;">star</span></label></article>          
-          
-        `;
-        break;
-        case "1":
-            alert("hola");
-            break;
-    }
+    fetch(links[i])
+    .then((response) => response.json())
+    .then((data) => pizarraInicio(data))
+  }
+  
+})
+
+
+selector.addEventListener("change", function () {
+  switch (selector.value) {
+    case "0":
+      ruta = links[0];
+      break;
+    case "1":
+      ruta = links[1];
+      break;
+    case "2":
+      ruta = links[2];
+      break;
+    case "3":
+      ruta = links[3];
+      break;
+    case "4":
+      ruta = links[4];
+      break;
+    case "5":
+      ruta = links[5];
+      break;
+    case "6":
+      ruta = links[6];
+      break;
+    case "7":
+      ruta = links[7];
+      break;
+    case "8":
+      ruta = links[8];
+      break;
+    case "9":
+      ruta = links[9];
+      break;
+    case "10":
+      ruta = links[10];
+      break;
+    case "11":
+      ruta = links[11];
+      break;
+    default:
+      ruta = links[0];
+      break;
+  }
+
+  fetch(ruta)
+    .then((response) => response.json())
+    .then((data) => actualizarPizarra(data));
 });
+
+function actualizarPizarra(data) {
+  if (Array.isArray(data)) {
+    pizarra.innerHTML = "";
+
+    for (i = 0; i < data.length; i++) {
+      horaActualizada(data[i])
+      pizarra.innerHTML += `
+      <article class="moneda">
+      <p class="title-article-moneda">${data[i].nombre.toUpperCase()}</p>
+      <img src="./img/united-states.png" class = "bandera">
+            <div class="precio">
+              <div class="precio-compra">
+                <p>COMPRA</p>
+              <p>$${data[i].compra}</p>
+              </div>
+              <div class="precio-venta">
+                <p>VENTA</p>
+              <p>$${data[i].venta}</p>
+              </div>
+              </div>
+          <input type="checkbox" id="star-checkbox6" class="input-star-checkbox"><label for="star-checkbox6" class="star"><span class="material-symbols-outlined" style="font-size: 28px;">star</span></label></article>
+      `;
+    }
+  } else {
+    pizarra.style.maxHeight = "45vh";
+    horaActualizada(data)
+    pizarra.innerHTML = `
+  <article class="moneda">
+ <p class="title-article-moneda">${data.nombre.toUpperCase()}</p>
+  <img src="${devolverBandera(data)}" class = "bandera">
+            <div class="precio">
+              <div class="precio-compra">
+                <p>COMPRA</p>
+              <p>$${data.compra}</p>
+              </div>
+              <div class="precio-venta">
+                <p>VENTA</p>
+              <p>$${data.venta}</p>
+              </div>
+              </div>
+          <input type="checkbox" id="star-checkbox6" class="input-star-checkbox"><label for="star-checkbox6" class="star"><span class="material-symbols-outlined" style="font-size: 28px;">star</span></label></article>
+  `;
+  }
+}
+
+function devolverBandera(data){
+  let bandera;
+  
+    switch(data.moneda){
+      case "USD":
+      bandera = "./img/united-states.png";
+      break;
+      case "EUR":
+      bandera = "./img/european-union.png";
+      break;
+      case "BRL":
+      bandera = "./img/brasil.png";
+      break;
+      case "CLP":
+      bandera = "./img/chile.png";
+      break;
+      case "UYU":
+      bandera = "./img/uruguay.png";
+      break;
+      default:
+      console.log("error")
+      break;
+  }
+
+  return bandera
+}
+
+function pizarraInicio(data) {
+  horaActualizada(data)
+  pizarra.innerHTML += `
+  <article class="moneda">
+ <p class="title-article-moneda">${data.nombre.toUpperCase()}</p>
+  <img src="${devolverBandera(data)}" class = "bandera">
+            <div class="precio">
+              <div class="precio-compra">
+                <p>COMPRA</p>
+              <p>$${data.compra}</p>
+              </div>
+              <div class="precio-venta">
+                <p>VENTA</p>
+              <p>$${data.venta}</p>
+              </div>
+              </div>
+          <input type="checkbox" id="star-checkbox6" class="input-star-checkbox"><label for="star-checkbox6" class="star"><span class="material-symbols-outlined" style="font-size: 28px;">star</span></label></article>
+  `;
+}
+
+function horaActualizada(data){
+  
+  const opciones = { 
+    hour: '2-digit',
+    minute: '2-digit'
+  };
+
+  let hora = new Date(data.fechaActualizacion).toLocaleString("es-ES", opciones);
+  
+  horaActualizacion.innerHTML = `Datos actualizados a las ${hora} hs`
+}
